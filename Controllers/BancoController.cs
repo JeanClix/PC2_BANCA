6b0e2abf838277216f48df1139e5c3108221ed2f
JeanClix <jp10.02.2003@gmail.com>
2024-09-28 00:44:35 -0500

    controller create

---
+++ Controllers/BancoController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PC2.Controllers
+{
+    [Route("[controller]")]
+    public class BancoController : Controller
+    {
+        private readonly ILogger<BancoController> _logger;
+
+        public BancoController(ILogger<BancoController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
